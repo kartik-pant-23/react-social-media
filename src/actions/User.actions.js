62fd5973a0bd171b9dfa4123abd6810877e743a8
Kartik Pant <pantkartik23@gmail.com>
2023-03-24 12:52:02 +0530

    Added welcome screen
    - Users list is fetched from the API, and a random user is selected as the current user.
    - Used redux for maintaining the state of the application.
    - Added thunk middleware for async actions.

---
+++ src/actions/User.actions.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const FETCH_USER_REQUEST = "fetch_user_request";
+const FETCH_USER_SUCCESS = "fetch_user_success";
+const FETCH_USER_FAILURE = "fetch_user_failure";
+const CHANGE_CURRENT_USER = "change_current_user";
+const CHANGE_CURRENT_USER_RANDOMLY = "change_current_user_randomly";
+
+const fetchUserRequest = () => {
+  return {
+    type: FETCH_USER_REQUEST,
+  };
+};
+
+const fetchUserSuccess = (usersData) => {
+  return {
+    type: FETCH_USER_SUCCESS,
+    payload: usersData,
+  };
+};
+
+const fetchUserFailure = (error) => {
+  return {
+    type: FETCH_USER_FAILURE,
+    payload: error,
+  };
+};
+
+export const loadUsersData = () => {
+  return async (dispatch) => {
+    dispatch(fetchUserRequest());
+    try {
+      const usersData = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      dispatch(fetchUserSuccess(usersData.data));
+    } catch (e) {
+      dispatch(fetchUserFailure(e.message || "Oops.. Something went wrong!"));
+    }
+  };
+};
+
+export const changeCurrentUserRandomly = () => {
+  return {
+    type: CHANGE_CURRENT_USER_RANDOMLY,
+  };
+};
+
+export const changeCurrentUser = (newUserId) => {
+  return {
+    type: CHANGE_CURRENT_USER,
+    payload: newUserId,
+  };
+};
+
+export {
+  FETCH_USER_REQUEST,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+  CHANGE_CURRENT_USER,
+  CHANGE_CURRENT_USER_RANDOMLY,
+};
--- src/screens/welcome/Welcome.js
+++ src/screens/welcome/Welcome.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { changeCurrentUserRandomly } from "../../actions/User.actions";
+import styles from "./Welcome.module.css";
 
 function Welcome() {
-  return <div>Welcome</div>;
+  const usersState = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (usersState.users) {
+      dispatch(changeCurrentUserRandomly());
+    }
+  }, [dispatch, usersState.users]);
+
+  const UserDetails = useMemo(() => {
+    if (usersState.currentUser != null) {
+      const { name, username } = usersState.currentUser;
+      return (
+        <div className={styles.userDetails}>
+          <h5 className={styles.currentUserName}>{name}</h5>
+          <h3 className={styles.currentUserUsername}>{username}</h3>
+        </div>
+      );
+    }
+  }, [usersState.currentUser]);
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Welcome</h1>
+        {usersState.currentUser && UserDetails}
+        {usersState.loading && <div>Fetching user details...</div>}
+        {usersState.currentUser && (
+          <button className={styles.getStartedButton}>Get Started</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
