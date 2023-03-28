5691955c9d0f77446f3b0fed7e64a76b803ee758
Kartik Pant <pantkartik23@gmail.com>
2023-03-28 12:54:49 +0530

    Added left section
    - Changed dashboard to have side navigation along with outlet for the main component.
    - Added styling to NavLink depending on whether it is active or not.

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 function PrivateRoute({ children }) {
   const usersState = useSelector((state) => state.users);
   console.log(usersState);
-  return usersState.currentUser ? children : <Navigate to='/' />;
+  return usersState.currentUser ? children : <Navigate to='/welcome' />;
 }
 
 export default PrivateRoute;
