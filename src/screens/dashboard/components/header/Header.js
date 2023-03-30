b3591c424b0cff63766b5e602efa1da15a18ebcb
Kartik Pant <pantkartik23@gmail.com>
2023-03-30 16:06:53 +0530

    added profile dropdown

--- src/screens/dashboard/components/header/Header.js
+++ src/screens/dashboard/components/header/Header.js
@@ -1,28 +1,51 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import TextBox from "./components/textBox";
 import ProfileCircleAvatar from "../../../../components/profileCircleAvatar";
 import { HEADER_CIRCLE_AVATAR_STYLES } from "./Header.utils";
-import "./Header.css";
+import styles from "./Header.module.css";
 
 function Header() {
   const usersState = useSelector((state) => state.users);
+  const [profileDropdownExpanded, setProfileDropdownExpanded] = useState(false);
+
+  const handleOpenProfileDropdown = () => {
+    setProfileDropdownExpanded((expanded) => !expanded);
+  };
+
+  const ProfileDropdown = useMemo(() => {
+    if (profileDropdownExpanded)
+      return (
+        <div className={styles.profileDropdown}>
+          <Link className={styles.link} to='profile'>
+            Profile
+          </Link>
+          <hr className={styles.divider} />
+          <Link className={styles.link} to='changeUser'>
+            Change User
+          </Link>
+        </div>
+      );
+  }, [profileDropdownExpanded]);
 
   const UserProfileBox = useMemo(() => {
     return (
       <ProfileCircleAvatar
         name={usersState.currentUser.name}
         styles={HEADER_CIRCLE_AVATAR_STYLES}
+        onClick={handleOpenProfileDropdown}
       />
     );
   }, [usersState.currentUser.name]);
 
   return (
-    <div className="header">
+    <div className={styles.header}>
       <h3>React Social App</h3>
       <TextBox />
       {UserProfileBox}
+      {ProfileDropdown}
     </div>
   );
 }
