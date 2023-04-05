a178328eafc8be97238d3e1094db145ffd0d27fe
Kartik Pant <pantkartik23@gmail.com>
2023-04-05 11:53:15 +0530

    Fix: changed profile avatar from button to div (#21)

--- src/components/profileCircleAvatar/ProfileCircleAvatar.js
+++ src/components/profileCircleAvatar/ProfileCircleAvatar.js
@@ -4,24 +4,13 @@ import PropTypes from "prop-types";
 import { getInitials } from "./ProfileCircleAvatar.utils";
 import DefaultStyles from "./ProfileCircleAvatar.module.css";
 
-function ProfileCircleAvatar({ styles, name, onClick }) {
+function ProfileCircleAvatar({ styles, name }) {
   const initials = getInitials(name);
 
-  if (onClick) {
-    styles = {
-      ...styles,
-      cursor: "pointer",
-    };
-  }
-
   return (
-    <button
-      className={DefaultStyles.container}
-      style={styles}
-      onClick={onClick}
-    >
+    <div className={DefaultStyles.container} style={styles}>
       {initials}
-    </button>
+    </div>
   );
 }
 
@@ -31,11 +20,6 @@ ProfileCircleAvatar.propTypes = {
 
 ProfileCircleAvatar.defaultProps = {
   styles: {},
-  onClick: null,
 };
 
 export default ProfileCircleAvatar;
-
-ProfileCircleAvatar.prototype = {
-  name: PropTypes.string,
-};
