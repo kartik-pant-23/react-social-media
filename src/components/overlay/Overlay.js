8bcab1eccc1c56b3f2975d4ca4b9493653849188
Kartik Pant <pantkartik23@gmail.com>
2023-04-05 12:47:24 +0530

    feature: overlay for dropdowns

---
+++ src/components/overlay/Overlay.js
@@ -0,0 +1,47 @@
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Overlay.module.css";
+
+function Overlay({
+  Content,
+  isOpen,
+  overlayStyles,
+  contentStyles,
+  shouldCloseOnOverlayClicked,
+  onClose,
+}) {
+  const handleOnOverlayClosed = useCallback(() => {
+    if (shouldCloseOnOverlayClicked) {
+      onClose();
+    }
+  }, [shouldCloseOnOverlayClicked, onClose]);
+
+  if (isOpen)
+    return (
+      <div
+        className={styles.overlay}
+        onClick={handleOnOverlayClosed}
+        style={{ ...overlayStyles }}
+      >
+        <div className={styles.content} style={{ ...contentStyles }}>
+          {Content}
+        </div>
+      </div>
+    );
+  return <></>;
+}
+
+Overlay.propTypes = {
+  Content: PropTypes.object.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+};
+
+Overlay.defaultProps = {
+  overlayStyles: {},
+  contentStyles: {},
+  shouldCloseOnOverlayClicked: false,
+  onClose: () => {},
+};
+
+export default Overlay;
--- src/screens/dashboard/components/header/Header.js
+++ src/screens/dashboard/components/header/Header.js
@@ -1,9 +1,10 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import TextBox from "./components/textBox";
 import ProfileCircleAvatar from "../../../../components/profileCircleAvatar";
+import Overlay from "../../../../components/overlay";
 import { HEADER_CIRCLE_AVATAR_STYLES } from "./Header.utils";
 import styles from "./Header.module.css";
 
@@ -11,24 +12,27 @@ function Header() {
   const usersState = useSelector((state) => state.users);
   const [profileDropdownExpanded, setProfileDropdownExpanded] = useState(false);
 
-  const handleOpenProfileDropdown = () => {
-    setProfileDropdownExpanded((expanded) => !expanded);
-  };
+  const handleOpenProfileDropdown = useCallback(() => {
+    setProfileDropdownExpanded(true);
+  }, []);
+
+  const handleCloseProfileDropdown = useCallback(() => {
+    setProfileDropdownExpanded(false);
+  }, []);
 
   const ProfileDropdown = useMemo(() => {
-    if (profileDropdownExpanded)
-      return (
-        <div className={styles.profileDropdown}>
-          <Link className={styles.link} to='profile'>
-            Profile
-          </Link>
-          <hr className={styles.divider} />
-          <Link className={styles.link} to='changeUser'>
-            Change User
-          </Link>
-        </div>
-      );
-  }, [profileDropdownExpanded]);
+    return (
+      <div className={styles.profileDropdown}>
+        <Link className={styles.link} to='profile'>
+          Profile
+        </Link>
+        <hr className={styles.divider} />
+        <Link className={styles.link} to='changeUser'>
+          Change User
+        </Link>
+      </div>
+    );
+  }, []);
 
   const UserProfileBox = useMemo(() => {
     return (
@@ -42,14 +46,23 @@ function Header() {
         />
       </button>
     );
-  }, [usersState.currentUser.name]);
+  }, [usersState.currentUser.name, handleOpenProfileDropdown]);
 
   return (
     <div className={styles.header}>
       <h3>React Social App</h3>
       <TextBox />
       {UserProfileBox}
-      {ProfileDropdown}
+      <Overlay
+        Content={ProfileDropdown}
+        isOpen={profileDropdownExpanded}
+        shouldCloseOnOverlayClicked={true}
+        onClose={handleCloseProfileDropdown}
+        contentStyles={{
+          top: "5rem",
+          right: "1rem",
+        }}
+      />
     </div>
   );
 }
